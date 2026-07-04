706e53b872ef242b17049570e914dc4d7155833d
Ryan Griffiths <ryanbgriffiths97@gmail.com>
2026-07-04 16:47:02 -0700

    All comments added to server & client

--- client.py
+++ client.py
@@ -12,41 +12,60 @@ import os               #For operating sys interface
 import threading 
 
 def receiveMessages(serverSocket):
+    """
+    Function for receiving messages while client is in the command prompt
+    """
+    
+    #Enter infinite loop for reveiving messages
     while True:
         try:
             response = serverSocket.recv(1024)
 
+            #Exit the program if user Quits and no response is received
             if not response:
                 os._exit(0)
 
+            #Output received messages
             print("\n" + response.decode("ascii"))
 
         except:
             break
 
     return
+#End receiveMessages()
 
 def join(sentence, serverSocket):
+    """Function for transmitting JOIN command to server"""
     serverSocket.send(sentence.encode("ascii"))
     return
+#End join() 
 
 def list(sentence, serverSocket):
+    """Function for transmitting LIST command to server"""
     serverSocket.send(sentence.encode("ascii"))
     return 
+#End list()
 
 def mesg(sentence, serverSocket):
+    """Function for transmitting MESG command to server"""
     serverSocket.send(sentence.encode("ascii"))
     return
+#End mesg() 
 
 def bcst(sentence, serverSocket):
+    """Function for transmitting BCST command to server"""
     serverSocket.send(sentence.encode("ascii"))
     return 
+#End bcst() 
 
 def log(sentence, serverSocket):
+    """Function for transmitting LOG command to server"""
     serverSocket.send(sentence.encode("ascii"))
     return
+#End log() 
 
 def main():
+    """Main Program Function"""
     
     #Validate command line argument provided, output usage upon discrepency and exit 
     if len(sys.argv) != 3:
@@ -61,7 +80,6 @@ def main():
 
     #Setup the socket and try to connect to the server
     serverSocket = socket(AF_INET, SOCK_STREAM)
-
     try:
         serverSocket.connect((serverName,serverPort))
         print(f"Connected to server {serverName}:{serverPort}")
@@ -70,6 +88,7 @@ def main():
         print("Could not connect to the server.")
         sys.exit(1)
 
+    #Create thread for incoming messages
     receiverThread = threading.Thread(target=receiveMessages, args=(serverSocket,))
     receiverThread.daemon = True
     receiverThread.start()
@@ -77,29 +96,39 @@ def main():
     #Get user input/commands 
     sentence = input("Enter JOIN followed by your username: ").strip()
     
+    #Variable for username storage 
     username = None
 
     #Loop until the user enters QUIT
     while sentence.upper() != "QUIT":
     
+        #Handle case where user hits Enter with no input
         if sentence ==  "":
             sentence = input("").strip()
             continue
 
+        #Store the command from the user
         command = sentence.split()[0]
 
+        #Determine & execute the respective function for the given command
         match command.upper():
             case "JOIN":
+                #Store the clients username
                 username = sentence.split()[1].strip()
                 join(sentence, serverSocket)
+
             case "LIST":
+
                 list(sentence, serverSocket)
             case "MESG":
                 mesg(sentence, serverSocket)
+
             case "BCST":
                 bcst(sentence, serverSocket)
+
             case "LOG":
                 log(sentence, serverSocket)
+
             case _: #Default-invalid command
                 print("Invalid command. Use JOIN <username>, LIST, MESG <username> <message>," \
                 "BCST <message>, LOG, or QUIT.")
@@ -110,8 +139,8 @@ def main():
     #Send QUIT to the server if entered by the user
     serverSocket.send("QUIT".encode('ascii'))
 
-    print(f"{username} is quitting the chat server")
-   
+    #Output status message
+    print(f"{username} is quitting the chat server") 
 
     #Close the connection 
     serverSocket.close()
--- server.py
+++ server.py
@@ -6,96 +6,135 @@
 #    
 # Usage: python server.py <port>
 
-import sys              #For command line args
-from socket import *    #For socket interface
-import threading        #For threading interface 
-import os               #For operating sys interface
-from datetime import datetime
-import time 
+import sys                      #For command line args
+from socket import *            #For socket interface
+import threading                #For threading interface 
+import os                       #For operating sys interface
+from datetime import datetime   #For timestamp
+import time                     #For time 
 
 #Global Variable to store addresses of registered clients
 registeredClients = {} #key = username, value = client socket.
 
+#Create mutex for threading
 lock = threading.Lock()
 
-def join(command, client_socket, address, serverDir):
-
-    #Obtain the remote address to which the socket is connected 
-    address = client_socket.getpeername()
-
+def join(command, client_socket, serverDir):
+    """
+    Function for JOIN command. 
+    Joins a user to the chat server, if not registered JOIN will register 
+    the user, already registered users are not re-entered into registration list.
+    If the server is at capacity the user will not join and will have to try again later.
+    NOTE: join() will conflict with other predifined Python interfaces if utilized.
+    """
+    #Save username provided
     username = command.split()[1]
 
+    #Protect against race conditions
     with lock:
-        #Already registed - send response 
+
+        #User Already registed -> send response & allow to join
         if username in registeredClients:
             client_socket.send("Already registered, enter next command".encode("ascii"))
             return True
-        #Server is full - send response 
+        
+        #Server is full -> send response & do not register
         elif len(registeredClients) >= 10:
             client_socket.send("Too Many Users".encode("ascii"))
             return False
+        
+        #Server has room and new user -> Register and allow to join
         else:
+            #Save the address for the user
             registeredClients[username] = client_socket
+
+            #Output status notification in server window
             print(f"{username} Joined the Chatroom")
+
+            #Send confirmation of Joining server to client
             client_socket.send(f"{username} joined! Connected to server!".encode("ascii"))
+            
+            #Initialize a log folder for the client
             fileName = os.path.join(serverDir, f"{username}.txt")
             with open(fileName, "a"):
                 pass
             return True
 
     return
+#End join()
 
 def list(client_socket, username):
-    # Registered client only - send a list of all registered clients on individual lines.
-    # return the list as a full block. 
+    """
+    Function for listing all registered users in the chat server.
+    client must be a registered user to execute LIST command.
+    """
 
+    #If client is not registered return error
     if username is None:
         client_socket.send("Error: Not registered.".encode("ascii"))
+    #Return list of current registered users in the chat server
     else:
         listOfClients = ",".join(registeredClients.keys())
         client_socket.send(listOfClients.encode("ascii"))
 
     return
+#End list()
 
 def mesg(command, client_socket, username, serverDir):
-    # Registered client only - client issues MESG <username> 'followed by the message.' to other
-    # registered client. Server relays to specified client.
-    # Check registration(connection) request from unregistered = "Unregistered User" & JOIN inst. 
-    # MESG to unregistered = "Unknown Recipient" back to client. 
+    """
+    Function for sending an individual message to another registered user.
+    Sender and receiver must bother be registered.
+    """
     
+    # Output status msg to server window & response to client upon 
+    #   request to message from an unregistered client
     if username not in registeredClients:
         print("Message request denied: Unregistered sender")
         client_socket.send("Unregistered User\nRegister via command: JOIN <username>".encode("ascii"))
         return
     
+    #Split the incoming comman 
     parts = command.split()
 
+    #Verify correct formatting of the MESG command 
     if len(parts) < 3:
         client_socket.send("Error: Invalid format.\nUsage: MESG <username> <message>".encode("ascii"))
         return
 
+    #Store sender and recipient usernames
     sender = username
     recipient = command.split()[1]
+
+    #Store the outgoing message
     message = command.partition(recipient)[2].strip()
     updatedMessage = sender + ": " + message
+
+    #Initializes paths for log files
     senderFile = os.path.join(serverDir, f"{username}.txt")
     recipientFile = os.path.join(serverDir, f"{recipient}.txt")
 
+    #Output status & send notifiction to sender if trying to 
+    #   message an unregistered user
     if recipient not in registeredClients:
         print("Message not sent: Unknown recipient")
         client_socket.send("Unknown Recipient".encode("ascii"))
         return 
+    #Output status, send message, & log the transaction
     else:
+        #Send message to recipient
         destinationSocket = registeredClients[recipient]
         destinationSocket.send(updatedMessage.encode("ascii"))
 
+        #Output status 
         print(f"Message sent to {recipient}")
         client_socket.send(command.encode("ascii"))
 
+        #Store clients info and message to log
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         senderLog = (f"[{timestamp}] INFO: Message sent by {sender} to {recipient} - \"{message}\"")
         recipientLog = (f"[{timestamp}] INFO: Message received by {recipient} from {sender} - \"{message}\"")
 
+        #Write logs to the respective client files
         with open(senderFile, "a") as log:
             log.write(senderLog + "\n")
 
@@ -103,28 +142,41 @@ def mesg(command, client_socket, username, serverDir):
             log.write(recipientLog + "\n")
 
     return
+#End mesg()
 
 def bcst(command, client_socket, username, serverDir):
-    # Broadcast msg to all other registered & not the sender. Must be reg to complete.
+    """    
+    Function for broadcasting a message to all other registered users. 
+    """
 
+    #Store the senders username & bcst message
     sender = username
-    message = command[4:].strip() #Remove bcst command 
-    updatedMessage = sender + ":" + message #append sender username to start of bcst
+    message = command[4:].strip() 
+
+    #Append the senders username to beginning of message
+    updatedMessage = sender + ":" + message 
+
+    #Initialize file path for logging
     senderFile = os.path.join(serverDir, f"{sender}.txt")
 
+    #Output status and send notification to sender if unregistered
     if sender not in registeredClients:
         print("Broadcast denied: Unregistered sender") 
         client_socket.send("Unregistered User\nRegister via command: JOIN <username>".encode("ascii"))
         return 
     
+    #Output verification of broadcast 
     client_socket.send((sender + " is sending a broadcast\n" + updatedMessage).encode("ascii"))
     
+    #Store information for logging
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     senderLog = (f"[{timestamp}] INFO: Broadcast sent by {sender} - \"{message}\"")
 
+    #Log the broadcast for the sender
     with open(senderFile, "a") as log:
         log.write(senderLog + "\n")
 
+    #Send and log the broadcast for the recipients
     for user, registered in registeredClients.items():
         if user != sender:
             registered.send(updatedMessage.encode("ascii"))
@@ -134,18 +186,24 @@ def bcst(command, client_socket, username, serverDir):
                 log.write(recipientLog + "\n")
 
     return
+#bcst()
 
 def log(command, client_socket, username, serverDir):
-    # Client retreval of history of all msgs sent and received during thier session. 
-    # format: timestamps, sender, receiver (if applicable), and message content.
-    # Save per user files as txt. 
+    """
+    Function for retreval of all messages sent and received during a client's session. 
+    Format: timestamps, sender, receiver (if applicable), and message content.
+    """
+
+    #Initialize file path variable for logging
     fileName = os.path.join(serverDir, f"{username}.txt")
     
+    #Output status msg & send notification with JOIN instructions 
+    #   if the client is not registered.
     if username not in registeredClients:
         print("Message Log denied: Unregistered Client")
         client_socket.send("Unregistered User\nRegister via command: JOIN <username>".encode("ascii"))
         return
-    
+    #Read & store the users log file and send the history back.
     else:
         with open(fileName, "r") as log:
             history = log.read()
@@ -154,17 +212,23 @@ def log(command, client_socket, username, serverDir):
         print(f"Chat Log sent for {username}")
 
     return 
+#log()
 
-# Function to handle program threading
-# Responsible for receiving commands from the individual threads 
+
 def threaded(client_socket, serverDir, address):
+    """
+    Function to handle program threading.
+    Responsible for receiving commands from the individual threads. 
+    """
 
+    #Variable for username storage
     username = None
 
+    #Set timeout & initialize variable for active user tracking
     client_socket.settimeout(1)
     lastActivity = time.time()
     
-    #Enter a loop to receive different commands
+    #Enter loop to receive different commands
     while True:
 
         try:
@@ -177,6 +241,7 @@ def threaded(client_socket, serverDir, address):
                 print('Disconnecting')
                 break
 
+            #Update last activity time 
             lastActivity = time.time()
 
             #Decode the response to save the entered command
@@ -187,37 +252,42 @@ def threaded(client_socket, serverDir, address):
 
             #Close the connection for registered client upon QUIT command
             if command.startswith('QUIT'):
+
+                #Remove the user from list of registered users
                 if username in registeredClients:
                     with lock:
                         del registeredClients[username]
                         otherUsers = registeredClients.copy().items()
 
+                    #Output status message in server window
                     print(f"Connection closed for {address}")
 
+                    #Send notification of user leaving to all other registered users
                     for user, registered in otherUsers:
                         try:
                             registered.send(f"{username} left".encode("ascii"))
                         except:
                             pass
                     break
+                #Close the connection for unregistered client
                 else: 
                     print(f"Connection closed for {address}")
                     break
         
             #Join the chat server if not already registered
             elif command.startswith("JOIN"):
-                if not join(command, client_socket, address, serverDir):
+                if not join(command, client_socket, serverDir):
                     client_socket.close()
                     return
                 else:
                     username = command.split()[1]
 
-            #List all registered clients in chat server upon receipt of the LIST command
+            #List all registered clients in chat server
             elif command.startswith("LIST"):
             
                 list(client_socket, username)
 
-            #Message a registered clients
+            #Message a registered client
             elif command.startswith("MESG"):
 
                 mesg(command, client_socket, username, serverDir)
@@ -227,29 +297,35 @@ def threaded(client_socket, serverDir, address):
 
                 bcst(command, client_socket, username, serverDir)
             
-            #List history of all messages sent and received upon receipt of LOG command
+            #List history of all messages sent and received 
             elif command.startswith("LOG"):
             
                 log(command, client_socket, username, serverDir)
 
-            #Send error message for any invalid commands
+            #Send error message for invalid commands
             else: 
                 client_socket.send("Error: Invalid command.".encode('ascii'))
 
+        #Handle user inactivity
         except timeout:
+
+            #Disconnect the client if inactive for one minute
             if time.time() - lastActivity >= 60:
                 print(f"{username}'s session is terminated due to inactivity for 1 minute.")
                 client_socket.send("Disconnected due to inactivity.\nGoodbye.".encode("ascii"))
                 
+                #Remove the client if registered
                 if username in registeredClients:
                     with lock:
                         del registeredClients[username]
                 
+                #Send notification to all registered clients of user disconnection
                 for user, registered in registeredClients.items():
                     if user != username:
                         registered.send(f"{username} was disconnected due to inactivity.".encode("ascii"))
                 break
 
+        #Handle connection reset if response is not received before disconnection
         except ConnectionResetError:
             print(f"Client {address} disconnected unexpectedly.")
 
@@ -269,6 +345,9 @@ def threaded(client_socket, serverDir, address):
 
 #Function to setup TCP connection
 def tcp_server(port):
+    """
+    Function for the setup of TCP connection used 
+    """
 
     #Create TCP socket
     server_socket = socket(AF_INET, SOCK_STREAM)
@@ -325,9 +404,12 @@ def tcp_server(port):
 #End tcp_server()
 
 
-#Main program function
+
 def main():
-    
+    """
+    Main Program Function 
+    """
+
     #Validation of comman line arguments provided, output usage upon descrepency
     if len(sys.argv) != 2:
         print("Usage: python server.py <port number>")
